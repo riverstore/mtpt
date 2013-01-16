a77a21c31754673a08d3539293641be29acff518
itxx00 <itxx00@gmail.com>
2013-01-16 23:32:12 +0800

    Update mtpt/aboutmtpt.php
    update the project url.

--- mtpt/aboutmtpt.php
+++ mtpt/aboutmtpt.php
@@ -6,7 +6,7 @@ stdhead(PROJECTNAME);
 print ("<h1>".PROJECTNAME."</h1>");
 begin_main_frame();
 
-print("<h1 align=center><a href=http://code.google.com/p/mtpt/ target=_blank>MTPT</a>  modified from <a href=http://sourceforge.net/projects/nexusphp/ target=_blank>Nexusphp</a><br />By 独行虾,星星 from<a href=http://www.xnlinux.cn target=_blank> XNLUG</a></h1>");
+print("<h1 align=center><a href=http://github.com/itxx00/mtpt/ target=_blank>MTPT</a>  modified from <a href=http://sourceforge.net/projects/nexusphp/ target=_blank>Nexusphp</a><br />By 独行虾,星星,EI.Nino from<a href=http://www.xnlinux.cn target=_blank> XNLUG</a></h1>");
 end_main_frame();
 stdfoot();  
 ?>
